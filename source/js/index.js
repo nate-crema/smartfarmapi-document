4e1d8c61c412fff35a23a62f940842bf8d6e865d
nate-crema <nate.crema@gmail.com>
2019-12-22 22:05:52 +0900

    edited background & login bugs

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,9 @@
 let login_timer;
+var tried = false;
+var cont = {
+    id_input: "",
+    pw_input: ""
+};
 
 
 $(document).ready(function() {
@@ -8,20 +13,28 @@ $(document).ready(function() {
         $(".guide-pw").css("opacity", "0");
     })
     $(".login-input").blur(function() {
-        var id = document.getElementById("login-id").value
-        var pw = document.getElementById("login-pw").value
+        var id = document.getElementById("login-id").value;
+        var pw = document.getElementById("login-pw").value;
         if (id == "") {
             $(".guide-id").css("opacity", "1");
         }
         if (pw == "") {
             $(".guide-pw").css("opacity", "1");
         }
-        if (id != "" && pw != "") {
+        if (id != "" && pw != "" && cont.id_input != id && cont.pw_input != pw) {
             $(".login-req-cover").css("opacity", "1");
             $(".timer-bar-inner").css("transition", "width 3s cubic-bezier(0,-0.01, 0.81, 1.04)");
             $(".timer-bar-inner").css("width", "0%");
-            login_start(3);
+            if (tried == false) {
+                login_start(3);
+            }
         }
+    })
+    $(".guide-pw").click(function() {
+        $("#login-pw").focus();
+    })
+    $(".guide-id").click(function() {
+        $("#login-id").focus();
     })
 })
 
@@ -30,24 +43,31 @@ $(document).ready(function() {
 function login_start(incounter) {
     document.getElementsByClassName("login-msg")[0].innerText = incounter + "초 후 로그인합니다.";
     setTimeout(() => {
-        if (document.getElementById('login-id') == document.activeElement || document.getElementById('login-id') == document.activeElement) {
+        if (document.getElementById('login-id') == document.activeElement || document.getElementById('login-pw') == document.activeElement) {
             $(".login-req-cover").css("opacity", "0");
             $(".timer-bar-inner").css("transition", "unset");
             $(".timer-bar-inner").css("width", "100%");
-        }
-        if (incounter != 0) login_start(incounter-1);
-        else if (incounter == 0) {
-            axios.post('/login', {
-                loginid: document.getElementById("login-id").value,
-                loginpw: document.getElementById("login-pw").value
-            })
-            .then(({data}) => {
-                console.log(data);
-                alert(data);
-            })
-            .catch((error) => {
-                alert("Error: " + error);
-            })
+        } else {
+            if (incounter != 0) login_start(incounter-1);
+            else if (incounter == 0) {
+                tried = true;
+                cont.id_input = document.getElementById("login-id");
+                cont.pw_input = document.getElementById("login-pw");
+                
+                axios.post('/login', cont)
+                .then(({data}) => {
+                    console.log(data);
+                    alert(data);
+                })
+                .catch((error) => {
+                    document.getElementsByClassName("login-msg")[0].innerText = "로그인중 에러가 발생하였습니다.";
+                    document.getElementsByClassName("login-msg")[0].style.color = "red";
+                    alert("Error: " + error);
+                    setTimeout(() => {
+                        location.reload();
+                    }, 1500);
+                })
+            }
         }
     }, 1000);
 }
