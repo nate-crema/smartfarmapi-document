d4ba021fd2594dcb32f6524b939bc3a2b8eb9978
nate-crema <nate.crema@gmail.com>
2019-12-23 14:48:12 +0900

    CORS acceptable setting

--- server/router.js
+++ server/router.js
@@ -11,4 +11,10 @@ module.exports = function(app, fs, path, axios, crypto) {
         console.log("/login accessed");
         res.end("true");
     })
+
+    app.all('/*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+    });
 }
