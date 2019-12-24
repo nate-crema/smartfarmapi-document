1973941a71cef3bd2ee8290fd08190524d95eeba
nate-crema <nate.crema@gmail.com>
2019-12-24 20:59:18 +0900

    update specific login & add page

---
+++ source/js/code.js
@@ -0,0 +1,14 @@
+// test data
+
+const session = {
+    name: "tester",
+    id: "test",
+    department: "Admin",
+    grade: "2",
+    level: "headmaster"
+}
+
+$(document).ready(function() {
+    $("body").css("width", $(window).width());
+    $("body").css("height", $(window).height());
+})
